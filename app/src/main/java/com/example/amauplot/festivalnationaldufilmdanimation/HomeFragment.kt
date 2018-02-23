95977fd71f4f5ff0f652e0a137316c05bb9e190e
Unknown <eliott.pincemaille@gmail.com>
2018-02-23 10:14:23 +0100

    Fix HomeFragment

--- app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/HomeFragment.kt
+++ app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/HomeFragment.kt
@@ -11,7 +11,18 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 
 class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance(events : ArrayList<LoadedData>, categories : Array<String>): HomeFragment {
+            val args = Bundle()
+            args.putParcelableArrayList("events", events)
+            args.putStringArray("categories", categories)
+            val fragment = HomeFragment()
             fragment.arguments = args
+            return fragment
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_home, container, false)
