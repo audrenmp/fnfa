2a4e9966430fd305562e03c4df354659b678442d
Unknown <audren.mp@gmail.com>
2018-03-12 11:02:33 +0100

    update

--- app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/HomeFragment.kt
+++ app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/HomeFragment.kt
@@ -21,8 +21,7 @@ class HomeFragment : Fragment() {
             return fragment
         }
     }
-
-
+    
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_home, container, false)
     }
