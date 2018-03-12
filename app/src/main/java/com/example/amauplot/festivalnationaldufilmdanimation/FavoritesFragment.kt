f99889d3cac9b28e987bb2722657b5ae6d1c7a6a
Unknown <audren.mp@gmail.com>
2018-03-12 15:35:23 +0100

    onclick fav

--- app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/FavoritesFragment.kt
+++ app/src/main/java/com/example/amauplot/festivalnationaldufilmdanimation/FavoritesFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.LinearLayout
 
 class FavoritesFragment : Fragment() {
@@ -52,5 +53,10 @@ class FavoritesFragment : Fragment() {
 
         val adapterMovieLong = MovieCalendarViewAdapter(movies)
         recyclerViewMovieLong.adapter = adapterMovieLong
+
+        val btnFav: ImageButton = this.activity.findViewById(R.id.favoris);
+        btnFav.setOnClickListener({
+            // ici
+        })
     }
 }
